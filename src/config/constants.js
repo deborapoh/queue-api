3004c2aacecf0400abd82e08917f6517a398ca81
Debora de Oliveira <deborapoh@gmail.com>
2021-08-23 00:19:49 -0300

    feat: add queue_url env

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,3 +2,5 @@ export const AWS_CREDENTIALS = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 }
+
+export const QUEUE_URL = process.env.QUEUE_URL
